5fb608358c0617b7d5cd1824bb48f5dccfed909e
Wung Jae Lee <wunglee2021@u.northwestern.edu>
2021-05-08 23:18:14 +0900

    c++ reference

--- learn_c++/learn_c++/main.cpp
+++ learn_c++/learn_c++/main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include "header1.h"
 #include "header2.h"
+using namespace header1;
+using header1::foo;
 
 namespace header1 {
 int func() {
@@ -19,7 +21,32 @@ int func() {
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    /* namespace */
+    /*
     std::cout << "Hello, World!\n";
     std::cout << header1::func() << std::endl;
+    std::cout << func() << std::endl;
+    std::cout << foo() << std::endl;
+    std::cout << header2::foo() << std::endl;
+     */
+    
+    int a = 3;
+    int& another_a = a;
+    
+    another_a = 5;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "another_a: " << another_a << std::endl;
+    
+    // int& ref = 4; // this is not valid
+    const int& ref2 = 4; // this is okay
+    
+    int arr[3] = { 1, 2, 3 };
+    int(&ref)[3] = arr;
+    
+    ref[0] = 2;
+    ref[1] = 3;
+    ref[2] = 1;
+    
+    std::cout << arr[0] << arr[1] << arr[2] << std::endl;
     return 0;
 }
